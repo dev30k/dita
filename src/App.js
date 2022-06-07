bf972b776cfe47719fe244d1c078e9454c7b234b
dev30k <develop_kk@outlook.com>
2022-06-07 18:22:20 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from "./screens/Navbar/Navbar";
+import Routess from "./Routess";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./screens/Home/Home";
+import DocumentViewer from "./screens/Viewer/DocumentViewer";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+        <BrowserRouter>
+            <Navbar />
+            <div className="xl:ml-80">
+                    <Routes>
+                        <Route path="/" element={<Home />}/>
+                        <Route path='/document' element={<DocumentViewer />} />
+                    </Routes>
+            </div>
+
+        </BrowserRouter>
+
+  </div>
   );
 }
 
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <section className="bg-coolGray-50 py-4">
+            <div className="container px-4 mx-auto">
+                <div className="flex flex-wrap justify-center -m-3">
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/5 p-3">
+                        <div className="p-8 bg-white border border-coolGray-100 rounded-md shadow-dashboard">
+                            <div className="mb-4 max-w-max  p-3 rounded-md">
+                                <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGV4YW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+                            </div>
+                            <h2 className="text-sm font-medium text-coolGray-300 text-center mb-1 ">ACS 441: <span className="text-sm font-sans text-gray-500">Artificial Intelligence</span> </h2>
+                            <p className="text-xs font-medium text-coolGray-100 text-center">Category : <span className="text-xs font-sans text-gray-500"> School of Science and Engineering</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+
+export default Home;
